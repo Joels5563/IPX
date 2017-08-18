f9c224ab573cff62a12c60c4056f951624308d94
joels <joels.zhou@cn.ipx.net>
2017-08-18 17:47:53 +0800

    Agent修改

---
+++ Agent/app/src/main/java/com/ipx/agent/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.ipx.agent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 登录界面
+ *
+ * @author joels on 2017/8/18
+ **/
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
